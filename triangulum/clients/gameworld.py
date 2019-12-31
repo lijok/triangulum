da03ce4ca2dee3543a0cfb14c80c4967a33a55f8
lijok <28689084+lijok@users.noreply.github.com>
2019-12-31 23:35:38 +0000

    remove unused import

--- triangulum/clients/gameworld.py
+++ triangulum/clients/gameworld.py
@@ -2,7 +2,7 @@ import json
 import aiohttp
 
 from triangulum.clients.http.base import HttpBaseClient
-from triangulum.clients.routing import URL, RootURL
+from triangulum.clients.routing import URL
 from triangulum.clients.util import get_session_key, find_token, timestamp
 from triangulum.controllers.gameworld.player import Player
 from triangulum.controllers.gameworld.farm_list import FarmList
