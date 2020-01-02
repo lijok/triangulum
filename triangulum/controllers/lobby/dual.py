2a68eea2028fcd99d69b014d8ff5f40b40f3e6af
lijok <28689084+lijok@users.noreply.github.com>
2020-01-02 03:00:32 +0000

    add dual docs

--- triangulum/controllers/lobby/dual.py
+++ triangulum/controllers/lobby/dual.py
@@ -8,6 +8,17 @@ class Dual(BaseController):
         super().__init__(action_handler=action_handler, controller='dual')
 
     async def add(self, avatar_identifier: int, consumers_id: str, avatar_name: str, email: str) -> dict:
+        """Add a dual player to the account
+
+        Args:
+            avatar_identifier: Name of the player
+            consumers_id: UNKNOWN *
+            avatar_name: UNKNOWN *
+            email: Email address of the player *
+
+        Returns:
+            dict
+        """
         return await self.invoke_action(
             action='add',
             params={
