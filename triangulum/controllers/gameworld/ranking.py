fe5908f20a12460470380481c12f3715994a178a
lijok <28689084+lijok@users.noreply.github.com>
2020-01-02 02:17:36 +0000

    add ranking docs

--- triangulum/controllers/gameworld/ranking.py
+++ triangulum/controllers/gameworld/ranking.py
@@ -9,6 +9,15 @@ class Ranking(BaseController):
         super().__init__(action_handler=action_handler, controller='ranking')
 
     async def get_kingdom_victory_points_with_treasures(self, start: int, end: int) -> dict:
+        """Get kingdom victory points rankings with treasures
+
+        Args:
+            start: Index of the first ranking you wish to receive back
+            end: Index of the last ranking you wish to receive back
+
+        Returns:
+            dict
+        """
         return await self.invoke_action(
             action='getKingdomVictoryPointsWithTreasures',
             params={
@@ -18,6 +27,14 @@ class Ranking(BaseController):
         )
 
     async def get_kingdom_stats(self, kingdom_id: int) -> dict:
+        """Get stats about a kingdom
+
+        Args:
+            kingdom_id: ID of the kingdom
+
+        Returns:
+            dict
+        """
         return await self.invoke_action(
             action='getKingdomStats',
             params={
@@ -30,8 +47,19 @@ class Ranking(BaseController):
         start: int,
         end: int,
         ranking_category: RankingCategory,
-        _id: None,  # Returns some info on the specific player selection in rankings board
+        _id: None,
     ) -> dict:
+        """Get ranking information for a given ranking category
+
+        Args:
+            start: Index of the first ranking you wish to receive back
+            end: Index of the last ranking you wish to receive back
+            ranking_category: Ranking category you wish to receive information about
+            _id: ID of an entity selection (through a UI mechanism) for which to return extra info
+
+        Returns:
+            dict
+        """
         params = {
             'start': start,
             'end': end,
@@ -47,6 +75,14 @@ class Ranking(BaseController):
         )
 
     async def get_ranking_average_points(self, ranking_category: RankingCategory) -> dict:
+        """Get average points for a ranking category
+
+        Args:
+            ranking_category: Ranking category to get information for
+
+        Returns:
+            dict
+        """
         return await self.invoke_action(
             action='getRankingAveragePoints',
             params={
@@ -56,6 +92,15 @@ class Ranking(BaseController):
         )
 
     async def get_rank_and_count(self, id: int, ranking_category: RankingCategory) -> dict:
+        """UNKNOWN *
+
+        Args:
+            id: UNKNOWN *
+            ranking_category: Ranking category
+
+        Returns:
+            dict
+        """
         return await self.invoke_action(
             action='getRankAndCount',
             params={
@@ -66,11 +111,21 @@ class Ranking(BaseController):
         )
 
     async def get_world_stats(self) -> dict:
+        """Get statistics about the game world such as the race and troops distributions
+
+        Returns:
+            dict
+        """
         return await self.invoke_action(
             action='getWorldStats'
         )
 
     async def get_kingdom_internal_ranking(self) -> dict:
+        """Get internal kingdom rankings
+
+        Returns:
+            dict
+        """
         return await self.invoke_action(
             action='getKingdomInternalRanking'
         )
