83909fb888765c47d4ff62a9bcad7ab3a520ed55
lijok <28689084+lijok@users.noreply.github.com>
2020-01-02 03:01:22 +0000

    add gameworld docs

--- triangulum/controllers/lobby/gameworld.py
+++ triangulum/controllers/lobby/gameworld.py
@@ -8,6 +8,12 @@ class Gameworld(BaseController):
         super().__init__(action_handler=action_handler, controller='gameworld')
 
     async def get_possible_new_gameworlds(self) -> dict:
+        """Get a list of possible new game worlds that can be joined
+        This does not include the game worlds that this account is already a part of
+
+        Returns:
+            dict
+        """
         return await self.invoke_action(
             action='getPossibleNewGameworlds'
         )
