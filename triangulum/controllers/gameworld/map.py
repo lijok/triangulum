ea7ea1574a865b84e9cb507961d744b1300bd3c5
lijok <28689084+lijok@users.noreply.github.com>
2020-01-02 01:06:03 +0000

    add map docs

--- triangulum/controllers/gameworld/map.py
+++ triangulum/controllers/gameworld/map.py
@@ -10,13 +10,23 @@ class Map(BaseController):
         super().__init__(action_handler=action_handler, controller='map')
 
     async def get_heatmap_maximums(self) -> dict:
+        """UNKNOWN *
+
+        Returns:
+            dict
+        """
         return await self.invoke_action(
             action='getHeatmapMaximums'
         )
 
     async def get_by_region_ids(self, region_id_collection: dict) -> dict:
-        """
-        Retrieves region data by region id
+        """Retrieves region data by region id
+
+        Args:
+            region_id_collection: List of region IDs to fetch the information for
+
+        Returns:
+            dict
         """
         return await self.invoke_action(
             action='getByRegionIds',
@@ -26,6 +36,15 @@ class Map(BaseController):
         )
 
     async def edit_map_markers(self, markers: List[Marker], field_message: FieldMessage) -> dict:
+        """Edit markers on a map cell
+
+        Args:
+            markers: List of Marker objects to be edited / added on the map cell
+            field_message: Message to be placed on the cell
+
+        Returns:
+            dict
+        """
         return await self.invoke_action(
             action='editMapMarkers',
             params={
@@ -37,9 +56,21 @@ class Map(BaseController):
     async def field_marker_minimize(
         self,
         cell_id: int,
-        is_global: int,
+        is_global: bool,
         minimize_state: FieldMarkerPersonalMinimized
     ) -> dict:
+        """UI ACTION
+        Minimizes / maximizes a field marker so that it's (not)shown by default next time
+        the player enters the map over the UI
+
+        Args:
+            cell_id: ID of the cell on which the marker is placed
+            is_global: Whether the toggle is global *
+            minimize_state: State of the toggle (minimzed/maximized)
+
+        Returns:
+            dict
+        """
         return await self.invoke_action(
             action='fieldMarkerMinimize',
             params={
@@ -49,7 +80,17 @@ class Map(BaseController):
             }
         )
 
-    async def field_marker_close(self, id: int, is_global: int) -> dict:
+    async def field_marker_close(self, id: int, is_global: bool) -> dict:
+        """UI ACTION
+        Closes a field marker so that it's no longer shown on the UI
+
+        Args:
+            id: ID of the marker
+            is_global: Whether the closure is global *
+
+        Returns:
+            dict
+        """
         return await self.invoke_action(
             action='fieldMarkerClose',
             params={
@@ -58,7 +99,16 @@ class Map(BaseController):
             }
         )
 
-    async def field_marker_delete(self, id: int, is_global: int) -> dict:
+    async def field_marker_delete(self, id: int, is_global: bool) -> dict:
+        """Delete a field marker
+
+        Args:
+            id: ID of the marker
+            is_global: Whether the deletion is global *
+
+        Returns:
+            dict
+        """
         return await self.invoke_action(
             action='fieldMarkerDelete',
             params={
@@ -68,6 +118,14 @@ class Map(BaseController):
         )
 
     async def get_kingdom_influence_statistics(self, king_id: int) -> dict:
+        """Get statistics about a kingdoms influence on surrounding map cells
+
+        Args:
+            king_id: Player ID of the king of the kingdom
+
+        Returns:
+            dict
+        """
         return await self.invoke_action(
             action='getKingdomInfluenceStatistics',
             params={
