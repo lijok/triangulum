1b977f3b423cad25761669686ab2f003f71ce9ee
lijok <28689084+lijok@users.noreply.github.com>
2020-01-02 03:09:04 +0000

    add sitter docs

--- triangulum/controllers/lobby/sitter.py
+++ triangulum/controllers/lobby/sitter.py
@@ -8,6 +8,17 @@ class Sitter(BaseController):
         super().__init__(action_handler=action_handler, controller='sitter')
 
     async def add(self, avatar_identifier: int, consumers_id: str, avatar_name: str, email: str) -> dict:
+        """Add a sitter to the game world
+
+        Args:
+            avatar_identifier: UNKNOWN *
+            consumers_id: UNKNOWN *
+            avatar_name: UNKNOWN *
+            email: UNKNOWN *
+
+        Returns:
+            dict
+        """
         return await self.invoke_action(
             action='add',
             params={
@@ -27,6 +38,19 @@ class Sitter(BaseController):
         can_send_resources: bool,
         can_buy_and_spend_gold: bool
     ) -> dict:
+        """Set the functions a sitter has access to
+
+        Args:
+            avatar_identifier: ID of your avatar on a particular game world
+            sitter_account_identifier: ID of the sitters avatar on a particular game world
+            can_raid_other_players: Toggle whether the sitter can raid other players
+            can_send_reinforcements: Toggle whether the sitter can send reinforcements
+            can_send_resources: Toggle whether the sitter can send resources
+            can_buy_and_spend_gold: Toggle whether the sitter can buy and spend gold
+
+        Returns:
+            dict
+        """
         return await self.invoke_action(
             action='setPermissions',
             params={
@@ -42,6 +66,15 @@ class Sitter(BaseController):
         )
 
     async def remove(self, avatar_identifier: int, sitter_account_identifier: int) -> dict:
+        """Remove a sitter
+
+        Args:
+            avatar_identifier: ID of your avatar on a particular game world
+            sitter_account_identifier: ID of the sitters avatar on a particular game world
+
+        Returns:
+            dict
+        """
         return await self.invoke_action(
             action='remove',
             params={
